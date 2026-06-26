e6b88396b157e5496a71243d452228b56d451e6c
mohhh20055-cell <mohhh20055@gmail.com>
2026-06-26 19:42:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -167,14 +167,6 @@ if (cluster.isMaster) {
         }
         next();
     });
-    const app = express();
-
-// ==== أضف هذا السطر ====
-app.set('trust proxy', true);
-// ======================
-
-app.use(compression({...}));
-
     // ===== تحسين اتصالات HTTP =====
     const httpAgent = new https.Agent({
         keepAlive: true,
