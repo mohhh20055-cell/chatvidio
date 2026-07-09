fa33ce6a2a81771c5b33b543b07337084f34bbb1
mohhh20055-cell <mohhh20055@gmail.com>
2026-07-09 07:36:18 +0100

    Delete middleware directory

--- middleware/index.js
+++
@@ -1,9 +0,0 @@
-// middleware/index.js
-const auth = require('./auth');
-
-// إعادة تصدير كل الدوال
-module.exports = {
-    authenticate: auth.authenticate,
-    authorize: auth.authorize,
-    checkBanned: auth.checkBanned
-};
