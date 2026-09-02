d0e9e018d9094fe7edd82d10e80da036dce1e382
mohhh20055-cell <mohhh20055-cell@users.noreply.github.com>
2026-09-02 10:20:30 +0000

    Fix build error from redundant variable declarations

--- routes/offer.js
+++ routes/offer.js
@@ -151,9 +151,6 @@ router.post('/offer/create', authenticate, authorize(['teacher']), upload.single
                 const [year, month, day] = datePart.split('-').map(Number);
                 const timeClean = timePart.replace('Z', '').split('+')[0].split('-')[0];
                 const [hours, minutes] = timeClean.split(':').map(Number);
-                const localDate = new Date(year, (month || 1) - 1, day || 1, hours || 0, minutes || 0);
-                const [year, month, day] = datePart.split('-').map(Number);
-                const [hours, minutes] = timePart.split(':').map(Number);
                 // Algeria is UTC+1. Create Date representing Algeria time, then convert to UTC.
                 offerDateUTC = new Date(Date.UTC(year, month - 1, day, hours - 1, minutes));
             } else if (offer_date) {
